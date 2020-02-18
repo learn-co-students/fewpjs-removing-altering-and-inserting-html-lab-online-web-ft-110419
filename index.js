f0384adf9a66fead532a082d390482f39e2e8a07
jessesbyers <jesse.s.byers@gmail.com>
2020-02-18 16:22:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove(document.querySelector("main"))
+
+let element = document.createElement('h1')
+let newHeader = document.body.appendChild(element)
+newHeader.id = "victory"
+newHeader.innerHTML = 'Jesse is the champion'
+// `Hi, ${content}!`
+
