986addd7bda435c5833445edf3e69f2e3e182359
Nethelbert <gilblasse@gmail.com>
2020-02-19 05:53:41 +0000

    Done.

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id','victory');
-newHeader.innerText = "Emmanuel is the champion"
+newHeader.innerHTML = "Emmanuel is the champion";
 document.body.appendChild(newHeader);
