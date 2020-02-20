9350060a1a7ffbc28668b7e69084e2c39601e932
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-20 05:31:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Anna is the champion!";
+newHeader.className = "victory";
