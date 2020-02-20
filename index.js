2a0f4d013aaea7293e447e5ae7d7f6ea819da1f9
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-02-20 02:08:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.appendChild(newHeader)
+
+let main = document.querySelector('main')
+main.remove()
+
