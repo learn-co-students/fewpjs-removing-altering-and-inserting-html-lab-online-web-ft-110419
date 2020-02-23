195ede7c1724d0b62d9040d970bbe45277cc153d
Ethan <ethan_gustafson@outlook.com>
2020-02-23 03:14:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your code here!
 
-let main = document.querySelector("main");
+let main = document.querySelector("main"); // selected the main html tag
 
-main.remove();
+main.remove(); // we can call remove on an element itself
 
-let newHeader = document.createElement("h1");
+let newHeader = document.createElement("h1"); // We created a new element h1
 
-newHeader.id = "victory";
+newHeader.id = "victory"; // set the new h1 id == victory
 
-document.body.appendChild(newHeader);
+document.body.appendChild(newHeader); // We have to append the child to the body
 
-newHeader.innerHTML = "YOUR-NAME is the champion"
+newHeader.innerHTML = "Ethan is the champion" // We can set what is in the h1 with innerHTML method 
