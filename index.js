37a0da8f135c3dd1d6b525387633d84d1db83dfa
Ethan <ethan_gustafson@outlook.com>
2020-02-23 03:12:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let main = document.querySelector("main");
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
